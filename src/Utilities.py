1bea65b0f48707e9113d05d88f2c43466d57a135
Matt Kershaw <matt@kershaw.org>
2022-06-04 02:42:12 +0100

    Fix sanitising

--- src/Utilities.py
+++ src/Utilities.py
@@ -1,4 +1,4 @@
 def sanitise(string, safe_chars=None):
     if safe_chars is None:
-        safe_chars = "?.\"'" + str.join('', range(ord('A'), ord('z') + 1))
+        safe_chars = "?.\"'" + str.join('', [chr(i) for i in range(ord('A'), ord('z') + 1)])
     return str.join('', [char for char in string if char in safe_chars])
