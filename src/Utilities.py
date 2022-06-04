4d320b8eb8f0f7c7c58b6f641af61f88d4c16d2f
Matt Kershaw <matt@kershaw.org>
2022-06-04 10:44:00 +0100

    Add default max length of 10k characters to sanitiser

--- src/Utilities.py
+++ src/Utilities.py
@@ -1,5 +1,5 @@
-def sanitise(string, safe_chars=None):
+def sanitise(string, safe_chars=None, max_length=10000):
     if safe_chars is None:
         alphabet = 'abcdefghijiklmnopqrstuvwyz'
         safe_chars = "?.\"' " + alphabet + alphabet.upper()
-    return str.join('', [char for char in string if char in safe_chars])
+    return str.join('', [char for char in string if char in safe_chars])[:max_length]
