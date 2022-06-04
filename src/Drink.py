62ac580e9e98becff90c75085222441ef6e122f2
Matt Kershaw <matt@kershaw.org>
2022-06-04 10:56:32 +0100

    Support off brand naming mistakes

--- src/Drink.py
+++ src/Drink.py
@@ -3,10 +3,10 @@ from functools import lru_cache
 
 
 def get_drink():
-    return random.choice(get_drinks())
+    return random.choice(random.choice(get_drinks()))
 
 
 @lru_cache(1)
 def get_drinks():
     with open('..\\data\\drinks.txt', 'r') as f:
-        return [line.strip('\n') for line in f.readlines()]
+        return [line.strip('\n').split(',') for line in f.readlines()]
