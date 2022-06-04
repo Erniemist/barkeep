233c56ef5d8f7653cc3605b13b4fb8d6efe54d1b
Matt Kershaw <matt@kershaw.org>
2022-06-04 10:41:36 +0100

    Add spaces to whitelisted characters

--- src/Utilities.py
+++ src/Utilities.py
@@ -1,5 +1,5 @@
 def sanitise(string, safe_chars=None):
     if safe_chars is None:
         alphabet = 'abcdefghijiklmnopqrstuvwyz'
-        safe_chars = "?.\"'" + alphabet + alphabet.upper()
+        safe_chars = "?.\"' " + alphabet + alphabet.upper()
     return str.join('', [char for char in string if char in safe_chars])
