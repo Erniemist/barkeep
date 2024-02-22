c2c4c54e693a6cf4c6968cf4ce01d4f89366fb49
Matt Kershaw <matt@kershaw.org>
2024-02-22 00:26:48 +0000

    Revert "Add Percival to the building"
    This reverts commit 6d8075ac8234baeb841bd5f95f9abcbfcb034ffc.

--- src/Avalon/Roles/Morgana.py
+++
@@ -1,9 +0,0 @@
-from src.Avalon.Roles.Role import Role
-
-
-class Morgana(Role):
-    team = Role.EVIL
-    name = "Morgana"
-
-    def __init__(self, player):
-        super().__init__(player)
--- tests/TestRoles.py
+++ tests/TestRoles.py
@@ -3,8 +3,6 @@ import unittest
 from src.Avalon.Roles.LoyalServant import LoyalServant
 from src.Avalon.Roles.Merlin import Merlin
 from src.Avalon.Roles.Minion import Minion
-from src.Avalon.Roles.Morgana import Morgana
-from src.Avalon.Roles.Percival import Percival
 
 
 class TestRoles(unittest.TestCase):
@@ -19,18 +17,6 @@ class TestRoles(unittest.TestCase):
         ]
         player = players[2]
         assert player.info(players) == "You are Merlin. Dan and Emily are Evil.", player.info(players)
-
-    def test_percival(self):
-        players = [
-            LoyalServant("Albert"),
-            Percival("Bernard"),
-            Merlin("Colin"),
-            Morgana("Dan"),
-            Minion("Emily")
-        ]
-        player = players[1]
-        assert player.info(
-            players) == "You are Percival. Colin and Dan are Merlin and Morgana, but you know not who is who..."
 
 
 if __name__ == '__main__':
