a6c67cbdfdf2dea4ae0099eb127af1da1ec0801d
Matt Kershaw <matt@kershaw.org>
2024-02-22 09:23:17 +0000

    Test Morgana and Loyal Servant

--- tests/TestRoles.py
+++ tests/TestRoles.py
@@ -8,7 +8,6 @@ from src.Avalon.Roles.Percival import Percival
 
 
 class TestRoles(unittest.TestCase):
-
     def test_merlin(self):
         players = [
             LoyalServant("Albert"),
@@ -19,6 +18,18 @@ class TestRoles(unittest.TestCase):
         ]
         player = players[2]
         assert player.info(players) == "You are Merlin. Dan and Emily are Evil.", player.info(players)
+
+    def test_loyal(self):
+        players = [
+            LoyalServant("Albert"),
+            LoyalServant("Bernard"),
+            Merlin("Colin"),
+            Minion("Dan"),
+            Minion("Emily")
+        ]
+        player = players[1]
+        assert player.info(players) == "You are a Loyal Servant of King Arthur of Britain. You don't know anything.",\
+            player.info(players)
 
     def test_percival(self):
         players = [
@@ -44,6 +55,19 @@ class TestRoles(unittest.TestCase):
         player = players[4]
         assert player.info(
             players) == "You are a Loathsome Minion of Mordred. You know that Dan is your dark ally.", player.info(
+            players)
+
+    def test_morgana(self):
+        players = [
+            LoyalServant("Albert"),
+            Percival("Bernard"),
+            Merlin("Colin"),
+            Morgana("Dan"),
+            Minion("Emily")
+        ]
+        player = players[3]
+        assert player.info(
+            players) == "You are Morgana. You know that Emily is your sinister accomplice.", player.info(
             players)
 
 
