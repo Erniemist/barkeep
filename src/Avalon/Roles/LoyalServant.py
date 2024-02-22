816ebdf83bff96c09f1f8ed53ba5424efdb7a9ea
Matt Kershaw <matt@kershaw.org>
2024-02-22 00:26:51 +0000

    Revert "Create Merlin Minion and Loyal Servant in some limited capacity"
    This reverts commit aa5da266083bc3e185a570ff0b990b25b7764dd2.

--- src/Avalon/Roles/LoyalServant.py
+++
@@ -1,6 +0,0 @@
-class LoyalServant:
-    team = "Good"
-    name = "Loyal Servant"
-
-    def __init__(self, player):
-        self.player = player
--- src/Avalon/Roles/Minion.py
+++
@@ -1,6 +0,0 @@
-class Minion:
-    team = "Evil"
-    name = "Loathsome Minion of Mordred"
-
-    def __init__(self, player):
-        self.player = player
