767d3af38c696b15d1fbbb902cc4953fa979e5bc
tinnyf <tinnyf2@gmail,com>
2024-02-25 22:32:28 +0000

    Allow conversion to ID and back

--- src/DiscordMember.py
+++ src/DiscordMember.py
@@ -3,6 +3,10 @@ import discord
 from src.DiscordMemberInterface import DiscordMemberInterface
 
 
+def from_id(member_id: int, guild: discord.Guild):
+    return DiscordMember(guild.get_member(member_id))
+
+
 class DiscordMember(DiscordMemberInterface):
 
     def __init__(self, member: discord.Member):
@@ -10,3 +14,6 @@ class DiscordMember(DiscordMemberInterface):
 
     async def send(self, message):
         await self.member.send_message(message)
+
+    def get_id(self):
+        return self.member.id
