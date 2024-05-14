46b33d6bd2edc76091a4f94ae770ea511689d528
Matt Kershaw <erniemist@gmail.com>
2024-05-14 01:41:51 +0100

    WIP: Add proposals
    (cherry picked from commit ff677b648428516099718add712be233260757c5)

--- src/avalon/game.py
+++ src/avalon/game.py
@@ -1,6 +1,8 @@
 import json
 import random
-from typing import Generator, Sequence
+from typing import Sequence
+
+from discord import Interaction
 
 from src.avalon.player import Player
 from src.avalon.Roles import role_factory
@@ -9,44 +11,59 @@ from src.discord.member.discord_member_interface import DiscordMemberInterface
 from src.discord.client.client_interface import ClientInterface
 
 
-class Game:
-    def __init__(
-        self, members: Sequence[DiscordMemberInterface], role_names: Sequence[str]
-    ):
-        self.players = [
-            Player(member, role_factory.from_name(role_name))
-            for member, role_name in zip(members, role_names)
-        ]
-
-    def get_info(self) -> Generator[tuple[Player, str], None, None]:
-        for player in self.players:
-            yield player, player.info(self.players)
-
-    def display_turn_order(self) -> str:
-        return "\n ".join([player.name for player in self.players])
-
-    def to_json(self) -> str:
-        return json.dumps({"players": [player.to_dict() for player in self.players]})
-
-
-def start_game(
-    members: Sequence[DiscordMemberInterface], role_names: Sequence[str]
-) -> Game:
+async def start_game(
+    interaction: Interaction,
+    members: Sequence[DiscordMemberInterface],
+    role_names: Sequence[str],
+) -> None:
     members, role_names = list(members), list(role_names)
     random.shuffle(members)
     random.shuffle(role_names)
-    game = Game(members, role_names)
+
+    players = make_players(members, role_names)
+
+    store_players(players)
+    await display_turn_order(interaction, players)
+    for player in players:
+        await player.discord_member.send(player.info(players))
+
+
+def make_players(
+    members: list[DiscordMemberInterface], role_names: list[str]
+) -> list[Player]:
+    return [
+        Player(member, role_factory.from_name(role_name))
+        for member, role_name in zip(members, role_names)
+    ]
+
+
+def store_players(players):
     with open("data/avalon/game.json", "w", encoding="utf-8") as file:
-        file.write(game.to_json())
-    return game
+        file.write(to_json(players))
 
 
-async def load_game(
+def to_json(players: list[Player]) -> str:
+    return json.dumps([player.to_dict() for player in players])
+
+
+async def display_turn_order(interaction: Interaction, players: list[Player]) -> None:
+    await interaction.response.send_message(
+        "The turn order is\n" + "\n ".join([player.name for player in players])
+    )
+
+
+async def load_players(
     client: ClientInterface,
-    game_data: str,
-) -> Game:
-    players = json.loads(game_data)["players"]
-    return Game(
+) -> list[Player]:
+    with open("data/avalon/game.json", "r", encoding="utf-8") as file:
+        game_data = file.readline()
+
+    return await from_json(client, game_data)
+
+
+async def from_json(client, game_data) -> list[Player]:
+    players = json.loads(game_data)
+    return make_players(
         [await client.find_member(player["member_id"]) for player in players],
         [player["role"] for player in players],
     )
