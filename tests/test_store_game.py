0cb7910cb203152ee46253e059239913f5903c88
Matt Kershaw <erniemist@gmail.com>
2024-05-16 00:45:53 +0100

    Reapply "Reapply "WIP: Add proposals""
    This reverts commit 4b1807ae01e4a2ef9a4a288226d3baf477d23060.

--- tests/test_store_game.py
+++ tests/test_store_game.py
@@ -2,13 +2,13 @@ import pytest
 
 from src.avalon.Roles.loyal_servant import LoyalServant
 from src.avalon.Roles.merlin import Merlin
-from src.avalon.game import Game, load_game
+from src.avalon.game import to_json, make_players, from_json
 from tests.mocks.client import Client
 from tests.mocks.discord_member import DiscordMember
 
 
 def test_storing():
-    game = Game(
+    players = make_players(
         [
             DiscordMember("Alice", 1),
             DiscordMember("Bob", 2),
@@ -19,22 +19,22 @@ def test_storing():
         ],
     )
 
-    expected = '{"players": [{"member_id": 1, "role": "Merlin"}, {"member_id": 2, "role": "Loyal Servant"}]}'  # pylint: disable=line-too-long
-    actual = game.to_json()
+    expected = '[{"member_id": 1, "role": "Merlin"}, {"member_id": 2, "role": "Loyal Servant"}]'
+    actual = to_json(players)
 
     assert actual == expected, actual
 
 
 @pytest.mark.asyncio
 async def test_loading():
-    game_data = '{"players": [{"member_id": 1, "role": "Merlin"}, {"member_id": 2, "role": "Loyal Servant"}]}'  # pylint: disable=line-too-long
+    game_data = '[{"member_id": 1, "role": "Merlin"}, {"member_id": 2, "role": "Loyal Servant"}]'
 
-    game = await load_game(Client(), game_data)
+    players = await from_json(Client(), game_data)
 
-    alice = game.players[0]
+    alice = players[0]
     assert alice.discord_member.get_id() == 1
     assert alice.role.name == "Merlin"
 
-    bob = game.players[1]
+    bob = players[1]
     assert bob.discord_member.get_id() == 2
     assert bob.role.name == "Loyal Servant"
