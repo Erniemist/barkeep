b1ab6a418a4fbf981580c778b99ac2a404b8f680
Matt Kershaw <erniemist@gmail.com>
2024-11-05 22:01:28 +0000

    Add chameleon

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import json
+import random
+from typing import Optional
+
 import discord
 
 from src import utilities, suggestions
@@ -7,7 +11,6 @@ from src.avalon.start_game_view import StartGameView
 from src.config import DISCORD_TOKEN, AVALON_ENABLED
 from src.discord.client.client import Client
 from src.drink import get_drink_repository
-
 
 client = Client(intents=discord.Intents.default())
 
@@ -33,6 +36,75 @@ async def drink(interaction: discord.Interaction):
 
 
 @client.tree.command()
+async def see_chameleon_wordsets(interaction: discord.Interaction):
+    """List current chameleon wordsets"""
+    with open('wordsets.txt', 'r') as f:
+        wordsets = json.load(f)
+    if len(wordsets) == 0:
+        await interaction.channel.send("No wordsets")
+    else:
+        await interaction.channel.send('\n'.join(f"{set_code}: {', '.join(words)}" for set_code, words in wordsets.items()))
+
+
+@client.tree.command()
+async def update_chameleon_wordsets(interaction: discord.Interaction, set_code: str, words: str):
+    """Add a chameleon wordset"""
+    try:
+        await add_wordset(set_code, words)
+        await interaction.channel.send(f"Added {set_code}")
+    except Exception as e:
+        await interaction.channel.send(f"Failed to add {set_code}: {repr(e)}")
+
+
+async def add_wordset(set_code: str, words: str):
+    with open('wordsets.txt', 'r') as f:
+        wordsets = json.load(f)
+
+    wordsets[set_code] = words.split(', ')
+
+    with open('wordsets.txt', 'w') as f:
+        json.dump(wordsets, f)
+
+
+@client.tree.command()
+async def play_chameleon(
+    interaction: discord.Interaction,
+    set_code: str,
+    member1: discord.Member,
+    member2: Optional[discord.Member],
+    member3: Optional[discord.Member],
+    member4: Optional[discord.Member],
+    member5: Optional[discord.Member],
+    member6: Optional[discord.Member],
+    member7: Optional[discord.Member],
+    member8: Optional[discord.Member],
+):
+    """Play a game of chameleon!"""
+    with open('wordsets.txt', 'r') as f:
+        wordsets = json.load(f)
+    if set_code not in wordsets.keys():
+        await interaction.response.send_message(f"Set {set_code} not found")
+        return
+
+    words = wordsets[set_code]
+
+    members = [
+        member
+        for member in [member1, member2, member3, member4, member5, member6, member7, member8]
+        if member is not None and member.id != client.user.id
+    ]
+    random.shuffle(members)
+    turn_order_display = str.join("\n", [f"{i + 1}: {member.mention}" for i, member in enumerate(members)])
+    await interaction.response.send_message(f"Words are {', '.join(words)}" + "\nTurn order:\n" + turn_order_display)
+
+    word = random.choice(words)
+    random.shuffle(members)
+    await members[0].send("You are the chameleon")
+    for member in members[1:]:
+        await member.send(f"The word is {word}")
+
+
+@client.tree.command()
 async def suggest(interaction: discord.Interaction, suggestion: str):
     """Make a suggestion to improve the bot"""
     suggestions.add_suggestion(utilities.sanitise(suggestion))
@@ -53,18 +125,17 @@ if AVALON_ENABLED:
             ),
         )
 
-
     @client.tree.command()
     async def propose_quest(interaction: discord.Interaction):
         """Propose a quest"""
         view = await proposal_view.make(interaction.client)
         await interaction.response.send_message(view=view)
 
-
     @client.tree.command()
     async def check_turn_order(interaction: discord.Interaction):
         """Check the turn order. Unaware of next quest sender"""
         players = await load_players(interaction.client)
         await display_turn_order(interaction, players)
+
 
 client.run(DISCORD_TOKEN)
